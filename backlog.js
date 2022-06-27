a20c3a626d5a6d9483e73f6de06d369f8e3ab7b5
Maik Engel <maik.engel7@gmail.com>
2022-06-27 17:12:36 +0200

    Backlog Layout is ready

--- backlog.js
+++ backlog.js
@@ -2,6 +2,12 @@ setURL('http://developerakademie.com/smallest_backend_ever')
 
 let users = [];
 
+
+/**
+ * This function loaded the smallest_backend js
+ * 
+ *  @param {*}
+ */
 async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
