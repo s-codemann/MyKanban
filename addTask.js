a7354c4b1f9fca33523190cd68da251f4f7b6918
s.codemann <s.codemann@protonmail.com>
2022-06-28 18:18:42 +0200

    backend working

--- addTask.js
+++ addTask.js
@@ -37,13 +37,15 @@ function inputToObject() {
     catergory: catergory,
     urgency: urgency,
     id: counter,
-    // createdAt: new Date().getTime(),
-    // catergory: "to-do",
+    createdAt: new Date().getTime(),
+    catergory: "to-do",
   };
+  console.log(task, counter);
 
   backend.setItem(`task${counter}`, task);
+  console.log(backend.getItem(`task${counter}`));
   counter++;
-  //   backend.deleteItem("counter");
+  backend.deleteItem("counter");
   backend.setItem("counter", counter);
   resetFor();
   console.log("ende");
@@ -64,7 +66,7 @@ async function init() {
   await downloadFromServer();
   document.querySelector("form").addEventListener("submit", (ev) => {
     ev.preventDefault();
-    inputToObject();
+    // inputToObject();
     window.location.href = "./index.html";
   });
 }
