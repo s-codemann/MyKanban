d70c41ab9e3fc7daa57ac29b61bff2a12a765256
Nareg-B-94 <naregb94@gmail.com>
2022-06-28 09:45:33 +0200

    oficiall v1

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
 fetch("./sidebar.html")
-  //fetches sidebar
-  .then((res) => res.text())
-  .then((text) => (document.getElementById("sidebar-cont").innerHTML = text));
+    //fetches sidebar
+    .then((res) => res.text())
+    .then((text) => (document.getElementById("sidebar-cont").innerHTML = text));
