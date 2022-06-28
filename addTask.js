f193119ff5681ab142c6b16205d0dddc2bcbdb0f
s.codemann <s.codemann@protonmail.com>
2022-06-28 16:48:03 +0200

    form works

--- addTask.js
+++ addTask.js
@@ -6,7 +6,11 @@ let catergory;
 let urgency;
 let createButton = document.getElementById("create");
 let resetButton = document.getElementById("cancel");
-resetButton.addEventListener("click", reset);
+resetButton.addEventListener("click", resetFor);
+let allInputElements = Array.from(document.querySelectorAll("[type]"))
+  .concat(Array.from(document.querySelectorAll("textarea")))
+  .concat(Array.from(document.querySelectorAll("select")));
+resetFor();
 function getCounter() {
   if (
     backend.getItem("counter") === null ||
@@ -25,7 +29,7 @@ function inputToObject() {
   catergory = document.getElementById("category").value;
   urgency = document.getElementById("urgency").value;
 
-  console.log(counter);
+  console.log("läuft", counter);
   let task = {
     title: title,
     date: date,
@@ -33,18 +37,32 @@ function inputToObject() {
     catergory: catergory,
     urgency: urgency,
     id: counter,
+    createdAt: new Date().getTime(),
   };
 
   backend.setItem(`task${counter}`, task);
   counter++;
   //   backend.deleteItem("counter");
   backend.setItem("counter", counter);
-  reset();
+  resetFor();
 }
 
-function reset() {
-  Array.from(document.querySelectorAll("[type]"))
-    .concat(Array.from(document.querySelectorAll("textarea")))
-    .concat(Array.from(document.querySelectorAll("select")))
-    .forEach((elem) => (elem.value = null));
+// function reset() {
+
+//     allInputElements.forEach((elem) => (elem.value = null));
+// }
+
+function resetFor() {
+  for (i = 0; i < allInputElements.length - 1; i++) {
+    allInputElements[i].value = null;
+    allInputElements[i].required = "true";
+  }
+}
+async function init() {
+  await downloadFromServer();
+  document.querySelector("form").addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    inputToObject;
+    window.location.href = "./index.html";
+  });
 }
