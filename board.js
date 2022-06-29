0fef5b3b7768d732f610995170e1e27e1a9b2fd1
s.codemann <s.codemann@protonmail.com>
2022-06-29 21:31:03 +0200

    draggable functionality added

--- board.js
+++ board.js
@@ -1,3 +1,5 @@
+let taskColumns = Array.from(document.querySelectorAll(".column"));
+let boardTasks = [...document.querySelectorAll(".task-cont")];
 console.log("board.js loaded");
 async function tasksToBoard() {
   tasks = backend.getItem("tasks");
@@ -5,23 +7,34 @@ async function tasksToBoard() {
     //task elements
     let taskCont = document.createElement("div");
     taskCont.classList.add("task-cont");
+    taskCont.id = "task-id" + task.id;
     let taskTitle = document.createElement("h4");
     taskTitle.classList.add("task-title");
     taskTitle.textContent = task.title;
     let taskDescription = document.createElement("p");
     taskDescription.classList.add("task-description");
     taskDescription.textContent = task.description;
+    //taskID
+
     //overflow toggle
     let overflowToggle = document.createElement("span");
     overflowToggle.textContent = "^";
-    overflowToggle.classList.add("overflow-toggle");
+    overflowToggle.classList.add("overflow-toggle", "rotated");
     overflowToggle.addEventListener("click", wholeTaskVisible);
     //draggable
-    task.draggable = true;
+    taskColumns.forEach((column) =>
+      column.addEventListener("dragover", (ev) => {
+        ev.preventDefault();
+        column.addEventListener("drop", taskDrop);
+        // this.style.cursor = "pointer";
+      })
+    );
+    taskCont.draggable = true;
+    taskCont.addEventListener("dragstart", taskDragStart);
     taskCont.append(taskTitle, taskDescription, overflowToggle);
     // taskCont.addEventListener("pointerover", textResizeGrow);
     // taskCont.addEventListener("pointerleave", textResizeShrink);
-    Array.from(document.querySelectorAll(".column"))
+    taskColumns
       .find((column) => column.id === task.boardColumn)
       .append(taskCont);
   });
@@ -29,6 +42,7 @@ async function tasksToBoard() {
 async function initBoard() {
   await downloadFromServer();
   tasksToBoard();
+  boardTasks = [...document.querySelectorAll(".task-cont")];
 }
 initBoard();
 function textResizeGrow(ev) {
@@ -56,4 +70,32 @@ function wholeTaskVisible(ev) {
   this.classList.toggle("rotated");
   this.parentElement.classList.toggle("show-everything");
   this.parentElement.parentElement.classList.toggle("overflow-scroll");
+  this.scrollIntoView();
+}
+
+function taskDragStart(ev) {
+  //   ev.preventDefault();
+  ev.dataTransfer.setData("text", this.id);
+}
+function taskDrop(ev) {
+  //   ev.preventDefault();
+  //   console.log(ev.type);
+  ev.preventDefault();
+  let transferId = ev.dataTransfer.getData("text");
+  this.append(document.getElementById(transferId));
+  setTaskCategories();
+}
+async function setTaskCategories() {
+  let tasksCopy = JSON.parse(JSON.stringify(tasks));
+  console.log(tasksCopy);
+  [...document.querySelectorAll(".task-cont")].forEach((task) => {
+    tasksCopy.find(
+      (clonedTask) => clonedTask.id === parseInt(task.id.replace(/[^0-9]/g, ""))
+    ).boardColumn = task.parentElement.id;
+  });
+  console.log(tasksCopy);
+  tasksCopy.forEach((entry) => console.log(entry.title, entry.boardColumn));
+  backend.setItem("tasks", tasksCopy).then(() => {
+    console.log("tasks saved");
+  });
 }
