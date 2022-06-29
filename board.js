917495aa4f7eda3a4d8c9a4229819c497f6593c8
s.codemann <s.codemann@protonmail.com>
2022-06-29 16:45:51 +0200

    design tests

--- board.js
+++ board.js
@@ -2,12 +2,25 @@ console.log("board.js loaded");
 async function tasksToBoard() {
   tasks = backend.getItem("tasks");
   tasks.forEach((task) => {
+    //task elements
     let taskCont = document.createElement("div");
     taskCont.classList.add("task-cont");
-    let taskTitle = document.createElement("span");
+    let taskTitle = document.createElement("h4");
+    taskTitle.classList.add("task-title");
     taskTitle.textContent = task.title;
-    let taskDescription = document.createElement("P");
-    taskCont.append(taskTitle, taskDescription);
+    let taskDescription = document.createElement("p");
+    taskDescription.classList.add("task-description");
+    taskDescription.textContent = task.description;
+    //overflow toggle
+    let overflowToggle = document.createElement("span");
+    overflowToggle.textContent = "^";
+    overflowToggle.classList.add("overflow-toggle");
+    overflowToggle.addEventListener("click", wholeTaskVisible);
+    //draggable
+    task.draggable = true;
+    taskCont.append(taskTitle, taskDescription, overflowToggle);
+    // taskCont.addEventListener("pointerover", textResizeGrow);
+    // taskCont.addEventListener("pointerleave", textResizeShrink);
     Array.from(document.querySelectorAll(".column"))
       .find((column) => column.id === task.boardColumn)
       .append(taskCont);
@@ -18,3 +31,29 @@ async function initBoard() {
   tasksToBoard();
 }
 initBoard();
+function textResizeGrow(ev) {
+  let colChildren = this.parentElement.children;
+
+  console.log();
+  [...colChildren].forEach(
+    (child) =>
+      (child.style.fontSize =
+        parseInt(getComputedStyle(this.parentElement).width) / 13 + "px")
+  );
+}
+
+function textResizeShrink(ev) {
+  let colChildren = this.parentElement.children;
+  console.log();
+  [...colChildren].forEach(
+    (child) =>
+      (child.style.fontSize =
+        parseInt(getComputedStyle(this.parentElement).width) / 14 + "px")
+  );
+}
+function wholeTaskVisible(ev) {
+  //   this.parentElement.style.height = "1000px";
+  this.classList.toggle("rotated");
+  this.parentElement.classList.toggle("show-everything");
+  this.parentElement.parentElement.classList.toggle("overflow-scroll");
+}
